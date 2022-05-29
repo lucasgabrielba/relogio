ec1cde4b00e759cf5413ede7fcca23ca77672235
Lucas Gabriel <lucasgabrielba@gmail.com>
2022-05-29 08:45:45 -0300

    arquivo script.js adcionado

---
+++ script.js
@@ -0,0 +1,7 @@
+const hourHand = document.querySelector('.hand.hours')
+const minutesHand = document.querySelector('.hand.minutes')
+const secondsHand = document.querySelector('.hand.seconds')
+
+const setClock = () => {
+
+}
